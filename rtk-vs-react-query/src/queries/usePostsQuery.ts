77ea7b29960ef25e7bdd03709184732d67e63c5d
Kamil Żmudzki <72665459+zmudzkikamil@users.noreply.github.com>
2025-01-20 01:22:49 +0100

    Delete rtk-vs-react-query directory

--- rtk-vs-react-query/src/queries/usePostsQuery.ts
+++
@@ -1,7 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchPosts } from "../services/fetchPosts";
-import { Post } from "../types/Post";
-
-export const usePostsQuery = () => {
-    return useQuery<Post[], Error>(['posts'], fetchPosts);
-};
